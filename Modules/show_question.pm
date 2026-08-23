c2c4f9cb8a7081e97fd4c5b3e69943215facdf73
Adam Kolany <adamkolany@byczysko.tail79f4cf.ts.net>
2026-08-23 12:07:24 +0200

    Move \circ right after \bullet, add placeholder box to transpose/star icons
    - Reordered misc-products group: bullet, circle, times, transpose, star
      (was bullet, times, transpose, circle, star).
    - #0^\top and #0^\ast previously showed only the bare "T"/"*" glyph via
      KaTeX; now use the same base-box + superscript layout (svg_box +
      .xmsub) as the □²/□³ icons, to visually match that these operate on
      the preceding expression like the other superscript-template buttons.

--- Modules/show_question.pm
+++ Modules/show_question.pm
@@ -301,10 +301,10 @@ print qq{</div>};
 
       print "<span class='algGrp'>";
       print qq{<button type="button" class="pbtn" data-ins="\\bullet"><span class='mathbtn' data-tex="\\bullet"></span></button>};
-      print qq{<button type="button" class="pbtn" data-ins="\\times"><span class='mathbtn' data-tex="\\times"></span></button>};
-      print qq{<button type="button" class="pbtn" data-ins="#0^\\top"><span class='mathbtn' data-tex="{}^\\top"></span></button>};
       print qq{<button type="button" class="pbtn" data-ins="\\circ"><span class='mathbtn' data-tex="\\circ"></span></button>};
-      print qq{<button type="button" class="pbtn" data-ins="#0^\\ast"><span class='mathbtn' data-tex="{}^\\ast"></span></button>};
+      print qq{<button type="button" class="pbtn" data-ins="\\times"><span class='mathbtn' data-tex="\\times"></span></button>};
+      print qq{<button type="button" class="pbtn" data-ins="#0^\\top"><span class="xmsub"><span class="base">$svg_box</span><span class="sup">⊤</span></span></button>};
+      print qq{<button type="button" class="pbtn" data-ins="#0^\\ast"><span class="xmsub"><span class="base">$svg_box</span><span class="sup">*</span></span></button>};
       print "</span>";
 
       print "</div>"; break();
